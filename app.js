0831276fe384212fab71af2e2452f3e9435df76f
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-05-27 10:01:18 -0700

    finished the js portion of the project in app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('dark mode');
+// select root element (html), toggle .dark-theme
+
+// select articles
+// iterate over, return article.post for each article
+
+// import moment.js
+// format date
+
+const toggleBtn = document.querySelector(".btn")
+const articlesContainer = document.querySelector(".articles")
+
+// select button
+// add click event listener
+toggleBtn.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark-theme")
+})
+
+const articlesData = articles
+    .map((article) => {
+        const { title, date, length, snippet } = article;
+        const formatDate = moment(date).format("MMMM Do, YYYY")
+        return `<article class="post">
+            <h2>${title}</h2>
+            <div class="post-info">
+                <span>${formatDate}</span>
+                <span>${length} min read</span>
+            </div>
+            <p>
+                ${snippet}
+            </p>
+            </article>`
+    })
+    .join('')
+    
+articlesContainer.innerHTML = articlesData
+// console.log('dark mode');
