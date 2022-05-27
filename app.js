f65803fa94b055a9759ff822d1fe16061a8e7c5b
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-05-27 10:09:08 -0700

    added the dark-theme styles in css file for the styling portion of this project

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-// select root element (html), toggle .dark-theme
-
-// select articles
-// iterate over, return article.post for each article
-
-// import moment.js
-// format date
-
 const toggleBtn = document.querySelector(".btn")
 const articlesContainer = document.querySelector(".articles")
 
 // select button
 // add click event listener
 toggleBtn.addEventListener("click", () => {
+    // select root element (html), toggle .dark-theme
     document.documentElement.classList.toggle("dark-theme")
 })
 
+
+// import moment.js
+// format date
 const articlesData = articles
     .map((article) => {
         const { title, date, length, snippet } = article;
         const formatDate = moment(date).format("MMMM Do, YYYY")
+        // select articles
+        // iterate over, return article.post 
+        // for each article
         return `<article class="post">
             <h2>${title}</h2>
             <div class="post-info">
@@ -31,6 +30,6 @@ const articlesData = articles
             </article>`
     })
     .join('')
-    
+
 articlesContainer.innerHTML = articlesData
 // console.log('dark mode');
